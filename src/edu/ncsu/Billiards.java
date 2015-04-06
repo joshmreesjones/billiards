6eef427e1f3c275a7d523a3863ec0147a687adef
jmreesjo <jmreesjo@ncsu.edu>
2015-04-06 16:51:48 -0400

    Added pool table backdrop

--- src/edu/ncsu/Billiards.java
+++ src/edu/ncsu/Billiards.java
@@ -1,40 +1,40 @@
 package edu.ncsu;
 
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
 import org.newdawn.slick.AppGameContainer;
 import org.newdawn.slick.BasicGame;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Image;
 import org.newdawn.slick.SlickException;
 
 public class Billiards extends BasicGame {
-	public Billiards(String gamename) {
-		super(gamename);
-	}
-
-	@Override
-	public void init(GameContainer gc) throws SlickException {}
-
-	@Override
-	public void update(GameContainer gc, int i) throws SlickException {}
-
-	@Override
-	public void render(GameContainer gc, Graphics g) throws SlickException {
-		g.drawString("Howdy!", 100, 100);
+	public Billiards() {
+		super("Billiards");
 	}
 
 	public static void main(String[] args) {
 		try {
-			AppGameContainer appgc;
-			appgc = new AppGameContainer(
-							new Billiards("Billards"));
-			appgc.setDisplayMode(640, 480, false);
-			appgc.start();
+			AppGameContainer app = new AppGameContainer(new Billiards());
+			app.setDisplayMode(640, 480, false);
+			app.start();
 		} catch (SlickException ex) {
-			Logger.getLogger(Billiards.class.getName()).log(
-											Level.SEVERE, null, ex);
+			ex.printStackTrace();
 		}
+	}
+
+	@Override
+	public void init(GameContainer container) throws SlickException {
+
+	}
+
+	@Override
+	public void update(GameContainer container, int delta) throws SlickException {
+
+	}
+
+	@Override
+	public void render(GameContainer container, Graphics g) throws SlickException {
+		Image table = new Image("res/pool.jpg");
+		table.draw(0, 0);
 	}
 }
