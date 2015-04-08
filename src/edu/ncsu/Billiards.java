898c82eec585159f3b1ee49ffb4ee0bc9d71aa86
jmreesjo <jmreesjo@ncsu.edu>
2015-04-07 21:16:53 -0400

    Added line drawing with mouse input

--- src/edu/ncsu/Billiards.java
+++ src/edu/ncsu/Billiards.java
@@ -15,11 +15,17 @@ import org.newdawn.slick.geom.Circle;
 import org.newdawn.slick.geom.Vector2f;
 
 public class Billiards extends BasicGame {
-	private static final int WINDOW_WIDTH  = 640;
-	private static final int WINDOW_HEIGHT = 480;
+	private static final int WINDOW_WIDTH  = 800;
+	private static final int WINDOW_HEIGHT = 440;
 
 	private Image tableBackground;
 	private PoolBall poolBall;
+
+	private float time;
+
+	private Vector2f dragStart;
+	private Vector2f dragEnd;
+	private boolean isMouseDragging;
 
 	public Billiards() {
 		super("Billiards");
@@ -37,20 +43,40 @@ public class Billiards extends BasicGame {
 
 	@Override
 	public void init(GameContainer container) throws SlickException {
-		tableBackground = new Image("res/pool.jpg");
+		tableBackground = new Image("res/pool-wide.png");
 		poolBall = new PoolBall(100, 100, 11);
+
+		time = 0;
+
+		isMouseDragging = false;
 	}
 
 	@Override
 	public void update(GameContainer container, int delta) throws SlickException {
-		/*
 		Input input = container.getInput();
 
 		if (input.isMouseButtonDown(Input.MOUSE_LEFT_BUTTON)) {
+			/*
 			poolBall.setX(input.getMouseX());
 			poolBall.setY(input.getMouseY());
+			*/
+
+			int mouseX = input.getMouseX();
+			int mouseY = input.getMouseY();
+
+			if (!isMouseDragging) {
+				dragStart = new Vector2f((float) mouseX, (float) mouseY);
+				dragEnd   = new Vector2f((float) mouseX, (float) mouseY);
+
+				isMouseDragging = true;
+			} else {
+				dragEnd.set((float) mouseX, (float) mouseY);
+			}
+		} else {
+			if (isMouseDragging) {
+				isMouseDragging = false;
+			}
 		}
-		*/
 
 		// update position of poolBall based on its velocity
 		Vector2f velocity = poolBall.getVelocity();
@@ -64,7 +90,6 @@ public class Billiards extends BasicGame {
 		poolBall.setY(poolBall.getY() + deltaY);
 
 		// decrease velocity
-		System.out.println(velocity);
 		if (velocity.length() < .003f) {
 			velocity.set(0, 0);
 		} else {
@@ -72,6 +97,8 @@ public class Billiards extends BasicGame {
 		}
 
 		poolBall.setVelocity(velocity);
+
+		time++;
 	}
 
 	@Override
@@ -83,5 +110,14 @@ public class Billiards extends BasicGame {
 		g.setColor(Color.white);
 		g.draw(poolBall);
 		g.fill(poolBall);
+
+		if (isMouseDragging) {
+			float startX = dragStart.getX();
+			float startY = dragStart.getY();
+			float endX = dragEnd.getX();
+			float endY = dragEnd.getY();
+
+			g.drawLine(startX, startY, endX, endY);
+		}
 	}
 }
