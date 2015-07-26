9539e6e792185d4a6d3baa815bc7e7d4e909aa67
jmreesjo <jmreesjo@ncsu.edu>
2015-07-26 11:59:42 -0400

    Added no collision setup

--- src/edu/ncsu/billiards/setups/NoCollisionSetup.java
+++ src/edu/ncsu/billiards/setups/NoCollisionSetup.java
@@ -6,9 +6,39 @@ import edu.ncsu.billiards.gameobjects.PoolBall;
 
 import java.util.ArrayList;
 
+import org.dyn4j.geometry.Vector2;
+
+/**
+ * A setup which should not result in a collision.
+ *
+ * Assuming window dimensions:
+ *     - Width:  872 pixels
+ *     - Height: 472 pixels
+ *
+ * and a pixels/meter scale of:
+ *     - 328 pixels/meter
+ *
+ * We obtain these values:
+ *     - distance between window edge and pool table: .111 meters
+ *     - width of cushions: .106 meters
+ *     - total window width: 2.659 meters
+ *     - total window height: 1.439 meters
+ */
 public class NoCollisionSetup extends Setup {
 	public ArrayList<Pocket> generatePockets() {
 		ArrayList<Pocket> pockets = new ArrayList<Pocket>();
+
+		Pocket pocket1 = new Pocket(2, .62f, new Vector2(-1, 0));
+		Pocket pocket2 = new Pocket(2, .82f, new Vector2(-1, 0));
+
+		pocket1.setDestination(pocket2);
+		pocket1.setTimeDifference(-0.2);
+
+		//pocket2.setDestination(pocket1);
+		pocket2.setTimeDifference(-0.2);
+
+		pockets.add(pocket1);
+		pockets.add(pocket2);
 
 		return pockets;
 	}
